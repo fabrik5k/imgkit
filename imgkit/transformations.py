a16d4d3d3ac6e31b2e0b9c516ac180230f79f642
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-08-31 10:28:05 -0300

    refactor: ensure function always return int
    BREAKING CHANGE: return type changed from int | float to int"

--- imgkit/transformations.py
+++ imgkit/transformations.py
@@ -1,7 +1,8 @@
-def expansao_de_pixel(pixel, limite_L, limite_H):
 def expansao_de_pixel(pixel: int, limite_L: int, limite_H: int) -> int:
   if pixel > limite_L and pixel < limite_H:
     result = 255 / (limite_H - limite_L) * (pixel - limite_L)
   else:
     result = pixel
+
+  result = int(result)
   return result
