f2da1159e2a5ee41316f0dd947d45e344114f125
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-08-31 20:21:58 -0300

    lint: add lint  tranformations.py

--- imgkit/transformations.py
+++ imgkit/transformations.py
@@ -3,13 +3,14 @@ from beartype import beartype
 
 @beartype
 def expansao_de_pixel(pixel: int, limite_L: int, limite_H: int) -> int:
-    args = {
-        "pixel": pixel, "limite_L": limite_L, "limite_H": limite_H
-    }
+    MIN_VALUE_PIXEL = 0
+    MAX_VALUE_PIXEL = 255
+    args = {'pixel': pixel, 'limite_L': limite_L, 'limite_H': limite_H}
     for name, value in args.items():
-        if not (0 <= value <= 255):
-            raise ValueError(f"Expected {name} to be in the range 0 to 255, but received {value}")
-
+        if not (MIN_VALUE_PIXEL <= value <= MAX_VALUE_PIXEL):
+            raise ValueError(
+                f'Expected {name} to be in the range 0 to 255, but received {value}'
+            )
     if pixel > limite_L and pixel < limite_H:
         result = 255 / (limite_H - limite_L) * (pixel - limite_L)
     else:
