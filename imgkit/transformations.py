4a18342d30f3a8bd73c998d9888f26c695bc5b15
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-08-31 10:29:58 -0300

    feat: add validation for typehint values

--- imgkit/transformations.py
+++ imgkit/transformations.py
@@ -1,3 +1,7 @@
+from beartype import beartype
+
+
+@beartype
 def expansao_de_pixel(pixel: int, limite_L: int, limite_H: int) -> int:
     if pixel > limite_L and pixel < limite_H:
         result = 255 / (limite_H - limite_L) * (pixel - limite_L)
