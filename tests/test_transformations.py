59c646c0aad659f5a2d6945a46a279214821581b
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-08-31 20:25:21 -0300

    test(expansao_de_pixel): add tests for the function

---
+++ tests/test_transformations.py
@@ -0,0 +1,57 @@
+import pytest
+from beartype.roar import BeartypeCallHintParamViolation
+
+import imgkit.transformations as tfm
+
+
+class TestExpansaoDePixel:
+    @pytest.mark.parametrize(
+        "pixel,limite_L,limite_H,resultado_esperado",
+        [
+            (99, 100, 110, 99),
+            (100, 100, 110, 100),
+            (101, 100, 110, 25),
+            (109, 100, 110, 229),
+            (110, 100, 110, 110),
+            (111, 100, 110, 111),
+        ]
+    )
+    def test_expansao_de_pixel_accept(self, pixel, limite_L, limite_H, resultado_esperado):
+        resultado = tfm.expansao_de_pixel(pixel, limite_L, limite_H)
+        assert resultado_esperado == resultado
+
+    @pytest.mark.parametrize(
+        "pixel,limite_L,limite_H",
+        [
+            ("99", 100, 110),
+            (100, 100.0, 110),
+            (['101'], 100, 110),
+            (109, 100, [110]),
+        ]
+    )
+    def test_expansao_de_pixel_raise_type_error(self, pixel, limite_L, limite_H):
+        with pytest.raises(BeartypeCallHintParamViolation):
+            resultado = tfm.expansao_de_pixel(pixel, limite_L, limite_H)
+            _ = resultado
+
+    @pytest.mark.parametrize(
+        "pixel,limite_L,limite_H,msg",
+        [
+            (-1, 100, 110, r"range 0 to 255.*-1"),
+            (256, 100, 110, r"range 0 to 255.*256"),
+
+            (101, -1, 110, r"range 0 to 255.*-1"),
+            (101, 256, 110, r"range 0 to 255.*256"),
+
+            (101, 100, -1, r"range 0 to 255.*-1"),
+            (101, 100, 256, r"range 0 to 255.*256"),
+
+            # limites inconsistentes
+            (101, 110, 110, r"must be strictly less.*received.*110.*110"),
+            (101, 111, 110, r"must be strictly less.*received.*111.*110"),
+        ]
+    )
+    def test_expansao_de_pixel_raise_value_error(self, pixel, limite_L, limite_H, msg):
+        with pytest.raises(ValueError, match=msg):
+            resultado = tfm.expansao_de_pixel(pixel, limite_L, limite_H)
+            _ = resultado
