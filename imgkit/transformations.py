af4aab10e440771e870e5433ec20f77e431a423a
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-08-31 10:28:45 -0300

    lint: add lint to function "expansao_de_pixel"

--- imgkit/transformations.py
+++ imgkit/transformations.py
@@ -1,8 +1,8 @@
 def expansao_de_pixel(pixel: int, limite_L: int, limite_H: int) -> int:
-  if pixel > limite_L and pixel < limite_H:
-    result = 255 / (limite_H - limite_L) * (pixel - limite_L)
-  else:
-    result = pixel
+    if pixel > limite_L and pixel < limite_H:
+        result = 255 / (limite_H - limite_L) * (pixel - limite_L)
+    else:
+        result = pixel
 
-  result = int(result)
-  return result
+    result = int(result)
+    return result
