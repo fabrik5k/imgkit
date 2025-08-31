d695ae4788e8a27841a798109fc315a9c5758857
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-08-31 10:50:59 -0300

    feat: add validation for pixels range in "expansao_de_pixel" function

--- imgkit/transformations.py
+++ imgkit/transformations.py
@@ -3,6 +3,13 @@ from beartype import beartype
 
 @beartype
 def expansao_de_pixel(pixel: int, limite_L: int, limite_H: int) -> int:
+    args = {
+        "pixel": pixel, "limite_L": limite_L, "limite_H": limite_H
+    }
+    for name, value in args.items():
+        if not (0 <= value <= 255):
+            raise ValueError(f"Expected {name} to be in the range 0 to 255, but received {value}")
+
     if pixel > limite_L and pixel < limite_H:
         result = 255 / (limite_H - limite_L) * (pixel - limite_L)
     else:
