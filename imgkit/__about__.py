5ce1b976ccbc41ffae1624ac25c61a6df2bcbc28
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-08-31 23:06:41 -0300

    chore: more simple __About__ file

--- imgkit/__about__.py
+++ imgkit/__about__.py
@@ -1,10 +1,1 @@
-from importlib.metadata import PackageNotFoundError, version
-
-pkg_name = 'imgkit'
-
-try:
-    pkg_version = version(pkg_name)
-    __version__ = pkg_version
-except PackageNotFoundError:
-    print('Package not found!!')
-    __version__ = '0.0.0'
+__version__ = '0.0.0'
