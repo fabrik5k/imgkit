e6c06583a359d125da58dc1ee4f1b1652747ccff
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-08-31 22:54:52 -0300

    chore: add __about__.py to manage package version metadata

---
+++ imgkit/__about__.py
@@ -0,0 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
+pkg_name = 'imgkit'
+
+try:
+    pkg_version = version(pkg_name)
+    __version__ = pkg_version
+except PackageNotFoundError:
+    print('Package not found!!')
+    __version__ = '0.0.1'
+
