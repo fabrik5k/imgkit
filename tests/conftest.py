59e3c754f8fa87ee3184cf813b02c175365e8b51
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-09-08 00:35:54 -0300

    test: add fixture object for imagem class (not ready)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest
+import numpy as np
 
 from pictokit.utils import gerar_imagem_aleatoria
 
 
 @pytest.fixture
 def image_obj():
-    args = {'x': 256, 'y': 256, 'dtype': 'uint8', 'max_value': None, 'seed': None}
+    args = {'x': 256, 'y': 256, 'channels': 3, 'max_value': None, 'seed': None}
     img = gerar_imagem_aleatoria(**args)
     return img
