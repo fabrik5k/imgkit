3294b5fd491989bb1578bc6717386ad7225e4267
Fabio Oliveira <fabio.oliveira1403@outlook.com>
2025-09-17 22:54:07 -0300

    chore: change try module to tests

--- try_module.py
+++ try_module.py
@@ -1,11 +1,10 @@
 from pictokit import Image
 
-img = Image(path='./.github/readme/img_threshold.png')
+img = Image(path='./img_test/digital_negative.png')
 print(img)
 # Apply contrast expansion with low and high limits and show histogram
-# img.contrast_expansion(low_limit=150, high_limit=250, hist=True)
 img.histogram()
-img.thresholding(A=1, T=150, hist=True)
+img.digital_negative(hist=True)
 
 # Show original and transformed images side by side
 img.compare_images()
